971cbfc7b6c7b67c19ca15ace5de39dfa082334c
mporogomankadde <tuhairweronnie@gmail.com>
2021-12-28 13:04:59 -0600

    bmr

--- CS105/Cs105Project/src/chapter6/Array.java
+++ CS105/Cs105Project/src/chapter6/Array.java
@@ -5,19 +5,10 @@ import java.util.Scanner;
 public class Array {
 
 	public static void main(String[] args) {
-		int[] r=new int[5];
+		int[] r={1,2,3,4,5,6};
+		List<Integer> al= new ArrayList<>(List.of(r));
 		Scanner input=new Scanner(System.in);
-		System.out.println("Enter value");
 
-		System.out.println();
-		for(int i=0;i<r.length;i++) {
-			int num=input.nextInt();
-			r[i]=num;
-			
-		}
-		
-		for(int j=0;j<r.length;j++)
-		System.out.print(r[j]);
 
 	}
 
